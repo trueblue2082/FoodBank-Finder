7b77eea9c3bec977a64aa32052d14e26f53f6673
Donald Cole <donaldcole0@gmail.com>
2021-03-29 19:25:56 -0500

    modified components folder

--- components/views/index.js
+++ components/views/index.js
@@ -1,6 +1,6 @@
 export { default as Home } from "./Home";
 export { default as Location} from "./Location";
 export { default as Appointment } from "./Appointment";
-export { default as ReviewConfirmAppointment } from "./Review & Confirm Appointment";
+export { default as ReviewConfirmAppointment } from "./ReviewConfirmAppointment";
 export { default as Confirmation } from "./Confirmation";
 
