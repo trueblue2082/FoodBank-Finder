3c3cc1734314cc7fd6fa85cf34dc48f66d03b6c0
Donald Cole <donaldcole0@gmail.com>
2021-03-30 21:35:06 -0500

    css changes

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,4 @@
 export default () => `
 <footer>
-  <a href="#"><i class="fa fa-facebook-official"></i></a>
-  <a href="#"><i class="fa fa-pinterest-p"></i></a>
-  <a href="#"><i class="fa fa-twitter"></i></a>
-  <a href="#"><i class="fa fa-flickr"></i></a>
-  <a href="#"><i class="fa fa-linkedin"></i></a>
 </footer>
 `;
--- components/views/Location.js
+++ components/views/Location.js
@@ -79,7 +79,7 @@ export default () => `
 <html>
 <body>
 
-<h1>My First Google Map</h1>
+
 
 <div id="googleMap" style="width:100%;height:400px;"></div>
 
