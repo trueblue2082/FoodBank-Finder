355e4dfadb11eff2fa4d934535b86cadaf37841a
Donald Cole <donaldcole0@gmail.com>
2021-04-11 13:27:58 -0500

    add Photo

--- components/views/About.js
+++ components/views/About.js
@@ -1,5 +1,6 @@
 
-export default () => `
+export default (st) =>
+`
 <div class="about-section">
   <h1>About Us Page</h1>
   <p>This app was created to provide a method of setting up an appointment to pick up grocery items from a foodbank. My desire is to make it easier for both foodbank volunteers and customers to better facilitate the process of distribution and pickup of orders. This app is designed to reduce the stress of long wait lines and frustrating checkout processes.</p>
@@ -10,7 +11,7 @@ export default () => `
 <div class="row">
   <div class="column">
     <div class="card">
-      <img src="https://github.com/trueblue2082/FoodBank-Finder/blob/main/lib/Photos/20170902_125847.jpg?raw=true" alt="Donald" style="width:100%">
+      <img src="https://github.com/trueblue2082/FoodBank-Finder/blob/main/lib/20181007_112045.jpg?raw=true" alt="Donald Cole" style="width:100%">
       <div class="container">
         <h2>Donald Cole</h2>
         <p class="title">CEO & Founder</p>
@@ -21,3 +22,4 @@ export default () => `
     </div>
   </div>
 `
+
--- store/About.js
+++ store/About.js
@@ -3,6 +3,8 @@ export default {
   page: "About",
   photos: [
     {
+      url: "https://github.com/trueblue2082/FoodBank-Finder/blob/main/lib/20181007_112045.jpg?raw=true",
+      title: "Image of Donald Cole",
     }
   ]
 }
